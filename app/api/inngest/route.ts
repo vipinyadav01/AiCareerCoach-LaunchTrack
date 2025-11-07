7a753b838371e845012cdbd9ece6c41c2c5e9701
Vipin Yadav <vipinyadav9m@gmail.com>
2025-11-07 23:02:03 +0530

    refactor: reorganize Inngest client initialization and update TypeScript paths for better module resolution

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,14 +1,8 @@
-import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/functions";
 
-
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    generateIndustryInsights,
-
-  ],
+  functions: [generateIndustryInsights],
 });
